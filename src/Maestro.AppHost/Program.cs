dd0410eb335658b2e9f9e664f0f5cfa98a9e25c7
Al Lamarre <alamarre@gmail.com>
2024-04-21 10:56:03 -0400

    wip

--- src/Maestro.AppHost/Program.cs
+++ src/Maestro.AppHost/Program.cs
@@ -1,3 +1,14 @@
-var builder = DistributedApplication.CreateBuilder(args);
+using Aspire.Hosting;
+using Projects;
 
+//using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+var db = builder.AddPostgres("pg")
+    .WithDataVolume()
+    .AddDatabase("postgresdb");
+
+var api = builder.AddProject<Projects.Maestro_Web>("api")
+    .WithReference(db);
+var blazor = builder.AddProject<Projects.Maestro_Web_Ui>("blazor");
 builder.Build().Run();
